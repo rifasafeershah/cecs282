18b5e58ef2d748187aac3b41fd5d0e19ff4e89e1
Rifa Safeer Shah <rifashah28@gmail.com>
2019-10-27 22:23:19 -0700

    Update instructor.cpp

--- assignment2/instructor.cpp
+++ assignment2/instructor.cpp
@@ -1,1 +1,8 @@
-
+Instructor Class:
+private:
+  int numOfCoursesTaught;
+  string name; //formatted as First Last (assume no middle name)
+  string status; //full-time or part-time or tenured
+  Course* courses; //pointer to array of courses
+public:
+  Instructor();
