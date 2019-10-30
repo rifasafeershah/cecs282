3be71623d7196473dfc497e697e812a6f3a054b3
Rifa Safeer Shah <rifashah28@gmail.com>
2019-10-30 13:25:45 -0700

    Update course.h

--- assignment2/course.h
+++ assignment2/course.h
@@ -3,3 +3,12 @@ private:
   int numOfEnrolled;
   string courseNumber; //four-digit integer
   string courseName; //e.g. CECS 282
+  string semester; //Fall, Winter, Spring, Summer
+  Time last_date_to_enroll; //time in secs
+  Students* students; //pointer to array of students
+public:
+  Course();
+  Course(string num, string name, string sem, Time last_date, Student* stdts, int numOfEnroll);
+  Course(const Course& c);
+  Course& operator=(const Course& c);
+  ~Course();
