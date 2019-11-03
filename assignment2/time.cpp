a4ebbb0fb2d1b8e9ea8bd71e0a4e952c39a60eb1
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-03 14:55:51 -0800

    Update time.cpp

--- assignment2/time.cpp
+++ assignment2/time.cpp
@@ -1,1 +1,39 @@
+#include "Time.h"
+#include <iostream>
+#include <ctime>
+using namespace std;
 
+Time :: Time()
+{
+  this -> secs = 0;
+}
+
+Time :: Time(time_t)
+{
+  this -> secs = t;
+}
+
+Time :: Time(const Time& t)
+{
+  (*this).secs = t.secs;
+}
+
+string Time :: toString() const
+{
+  return ctime(&secs);
+}
+
+string Time :: getMMDDYYYY() const
+{
+  struct tm* nowLocal;
+  char buffer[80];
+  nowLocal = localtime(&secs);
+  strftime(buffer, 80, "%m%d%y", nowLocal);
+  return buffer;
+}
+
+string Time :: getYear() const
+{
+  struct tm* nowLocal;
+  char buffer[80];
+  
