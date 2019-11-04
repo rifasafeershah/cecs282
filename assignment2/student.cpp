d82698722e5fdd30baa840f71a2588bc39adb28f
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-03 23:03:41 -0800

    Update student.cpp

--- assignment2/student.cpp
+++ assignment2/student.cpp
@@ -1,10 +1,12 @@
-#include "Student.h"
 #include <iostream>
 #include <ctime>
-#include <time.h>
+#include "Student.h"
+#include "Time.h"
 
+using namespace std;
 
-Student :: Student()//default constructor
+/* Default Constructor */
+Student:: Student()
 {
 	level = "Recruit";
 	id = "000000000";
@@ -12,56 +14,70 @@ Student :: Student()//default constructor
 	status = "money";
 	date_of_action = 0;
 }
-Student :: Student(string id, string name, string level, Time date_of_action)//overloaded constructor
+
+/* Overload Constructor */
+Student :: Student(string id, string name, string level, Time date_of_action)
 {
-	this ->id = id;
-	this ->name = name;
-    this ->level = level;
-    this ->date_of_action = date_of_action;
+	this -> id = id;
+	this -> name = name;
+	this -> level = level;
+	this -> date_of_action = date_of_action;
 }
 
-Student :: Student(const Student& s)//copy constructor
+
+/* Copy Constructor */
+Student::Student(const Student& s)
 {
-  (*this).name = s.name;
-  (*this).id = s.id;
-  (*this).level = s.level;
-  (*this).date_of_action = s.date_of_action;
+	(*this).name = s.name;
+	(*this).id = s.id;
+	(*this).level = s.level;
+	(*this).date_of_action = s.date_of_action;
 }
 
-Student& Student :: operator=(const Student& s) //assignment operator
+/* Assignment Operator */
+Student& Student :: operator=(const Student& s)
 {
-  (*this).name = s.name;
-  (*this).id = s.id;
-  (*this).level = s.level;
-  (*this).date_of_action = s.date_of_action;  
-  return (*this);
+	(*this).name = s.name;
+	(*this).id = s.id;
+	(*this).level = s.level;
+	(*this).date_of_action = s.date_of_action;
+	return (*this);
 }
 
-string Student :: getLevel() const{
-  return level;
+/* Get Level */
+string Student::getLevel() const {
+	return level;
 }
 
-string Student :: getId() const{
-  return id;
+/* Get ID */
+string Student::getId() const {
+	return id;
 }
 
-string Student :: getName() const{
-  return name;
+/* Get Name */
+string Student::getName() const {
+	return name;
 }
 
-string Student :: getDateOfAction() const{
-  return date_of_action.toString();
+/* Get Date of Action */
+string Student::getDateOfAction() const {
+	return date_of_action.toString();
 }
 
-Time Student:: getTimeOfAction() const{//return date_of_action in seconds
-  return date_of_action;
+/* Get Time of Action */
+Time Student::getTimeOfAction() const {//return date_of_action in seconds
+	return date_of_action;
 }
-string Student :: getStatus() const{
-  return status;
+
+/* Get Status */
+string Student::getStatus() const {
+	return status;
 }
-void Student ::  setStatus(string stat){
-  status = stat;
+
+void Student::setStatus(string stat) {
+	status = stat;
 }
-void Student :: setTimeOfAction(Time act){
-  date_of_action = act;
+
+void Student::setTimeOfAction(Time act) {
+	date_of_action = act;
 }
