d67b6e59a507534317c0af0213db5d68eaeccf61
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-03 23:04:23 -0800

    Update time.cpp

--- assignment2/time.cpp
+++ assignment2/time.cpp
@@ -1,51 +1,62 @@
-#include "Time.h"
 #include <iostream>
 #include <ctime>
+#include "Time.h"
 using namespace std;
 
-Time :: Time()
+/* Default Time Constructor */
+Time::Time()
 {
-  this -> secs = 0;
+	secs = 0;
 }
 
-Time :: Time(time_t)
+/* Overload Time Constructor */
+Time::Time()
 {
-  this -> secs = t;
+	this->secs = 0;
 }
 
+/* Constructor */
+Time::Time(time_t t)
+{
+	this->secs = t;
+}
+
+/* Copy Constructor */
 Time :: Time(const Time& t)
 {
-  (*this).secs = t.secs;
+	(*this).secs = t.secs;
 }
 
-string Time :: toString() const
+string Time::toString() const
 {
-  return ctime(&secs);
+	return ctime(&secs);
 }
 
-string Time :: getMMDDYYYY() const
+/* Get MMDDYYYY */
+string Time::getMMDDYYYY() const
 {
-  struct tm* nowLocal;
-  char buffer[80];
-  nowLocal = localtime(&secs);
-  strftime(buffer, 80, "%m%d%y", nowLocal);
-  return buffer;
+	struct tm* nowLocal;
+	char buffer[80];
+	nowLocal = localtime(&secs);
+	strftime(buffer, 80, "%m%d%y", nowLocal);
+	return buffer;
 }
 
-string Time :: getYear() const
+/* Get Year */
+string Time::getYear() const
 {
-  struct tm* nowLocal;
-  char buffer[80];
-  nowLocal = localtime(&secs);
-  strftime(buffer, 80, "%y", nowLocal);
-  return buffer;
+	struct tm* nowLocal;
+	char buffer[80];
+	nowLocal = localtime(&secs);
+	strftime(buffer, 80, "%y", nowLocal);
+	return buffer;
 }
 
-int Time :: compareTime(const Time &t)
+/* Compare Time */
+int Time::compareTime(const Time& t)
 {
-  int difference = secs - t.secs;
-  if (difference == 0)
-  {
-    return 0;
-  }
-  
+	int difference = secs - t.secs;
+	if (difference == 0)
+	{
+		return 0;
+	}
