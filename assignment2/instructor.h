06f51533baeea959d18ad0b53dbfbf19bf7f4fe6
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-03 23:02:44 -0800

    Update instructor.h

--- assignment2/instructor.h
+++ assignment2/instructor.h
@@ -1,21 +1,31 @@
-Instructor Class:
-private:
-  int numOfCoursesTaught;
-  string name; //formatted as First Last (assume no middle name)
-  string status; //full-time or part-time or tenured
-  Course* courses; //pointer to array of courses
-public:
-  Instructor();
-  Instructor(string name, string status, Course* crs, int num);
-  Instructor(const Instructor& i);
-  ~Instructor();
-  Course* getCourse() const;
-  int getNumberOfCoursesTaught() const;
-  string getName() const;
-  string getStatus() const; //Part-time or Full-time or Tenured
-  string getStudentStatus(const Strudent& s, const Course& c) const; //Enrolled, Added, or Dropped
-  int addStudent(const Student& s, Course& c); //return -1 if a student already exists; return 0 if a student is not on the roster. Otherwise return 1
-  int dropStudent(const Student& s, Course& c, Time t); //return 0 if a student is not on the roster. Otherwise, return 1.
-  int findStudent(const Student& s, const Course& c); //return 0 if a student is not found. Otherwise, return 1.
-  int addCourse(const Course& c); //return -1 if course already exists; return 0 if the numOfCoursesTaught reaches MAXCOURSE. Otherwise, add the course and return 1.
-  int findCourse(const Course& c); //return 0 if a course is not found. Otherwise, return 1.
+#include <string>
+#include "Course.h"
+#include "Student.h"
+#include "Time.h"
+
+using namespace std;
+
+class Instructor {
+	private:
+
+		int numOfCoursesTaught;
+		string name; //formatted as First Last (assume no middle name)
+		string status; //full-time or part-time or tenured
+		Course* courses; //pointer to array of courses
+
+	public:
+		Instructor();
+		Instructor(string name, string status, Course* crs, int num);
+		Instructor(const Instructor& i);
+		~Instructor();
+		Course* getCourse() const;
+		int getNumberOfCoursesTaught() const;
+		string getName() const;
+		string getStatus() const; //Part-time or Full-time or Tenured
+		string getStudentStatus(const Student& s, const Course& c) const; //Enrolled, Added, or Dropped
+		int addStudent(const Student& s, Course& c); //return -1 if a student already exists; return 0 if a student is not on the roster. Otherwise return 1
+		int dropStudent(const Student& s, Course& c, Time t); //return 0 if a student is not on the roster. Otherwise, return 1.
+		int findStudent(const Student& s, const Course& c); //return 0 if a student is not found. Otherwise, return 1.
+		int addCourse(const Course& c); //return -1 if course already exists; return 0 if the numOfCoursesTaught reaches MAXCOURSE. Otherwise, add the course and return 1.
+		int findCourse(const Course& c); //return 0 if a course is not found. Otherwise, return 1.
+};
