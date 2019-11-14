5d9676215079c7c03087eac1e2781f8798a0fb65
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-13 22:27:27 -0800

    Update time.cpp

--- assignment2/time.cpp
+++ assignment2/time.cpp
@@ -1,71 +1,82 @@
+/*
+ * Name: Rifa Safeer Shah
+ * CECS 282 Assignment 2
+ * Last Updated 
+*/
+
 #include <iostream>
-#include <ctime>
 #include "Time.h"
+#include <ctime>
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+
 using namespace std;
 
-/* Default Time Constructor */
+/* Default Constructor */
 Time::Time()
 {
 	secs = 0;
-} //ends Default Time Constructor
+} //ends Default Constructor
 
-/* Overload Time Constructor */
-Time::Time()
-{
-	this->secs = 0;
-} //ends Overload Time Constructor
-
-/* Constructor */
+/* Overload Constructor */
 Time::Time(time_t t)
 {
 	this->secs = t;
-} //ends Constructor
+} //ends Overload Contructor
 
 /* Copy Constructor */
 Time :: Time(const Time& t)
 {
 	(*this).secs = t.secs;
-}
-
-string Time::toString() const
-{
-	return ctime(&secs);
-}
+} //ends Copy Constructor
 
 /* Get MMDDYYYY */
 string Time::getMMDDYYYY() const
 {
-	struct tm* nowLocal;
-	char buffer[80];
-	nowLocal = localtime(&secs);
-	strftime(buffer, 80, "%m%d%y", nowLocal);
-	return buffer;
-}
+	ostringstream date;
+	tm* gmtm = gmtime(&secs);
+	int month = gmtm->tm_mon + 1;
+	int day = gmtm->tm_mday;
+	int year = gmtm->tm_year + 1900;
+	date << month << "/" << day << "/" << year;
+	return date.str();
+} //ends Get MMDDYYYY
 
 /* Get Year */
 string Time::getYear() const
 {
-	struct tm* nowLocal;
+	time_t currentTime = secs;
+	struct tm* timeinfo;
+	time(&currentTime);
+	timeinfo = localtime(&currentTime);
 	char buffer[80];
-	nowLocal = localtime(&secs);
-	strftime(buffer, 80, "%y", nowLocal);
+	strftime(buffer, 80, "%G", timeinfo);
 	return buffer;
-}
+} //ends Get Year
 
 /* Compare Time */
 int Time::compareTime(const Time& t)
 {
-	int difference = secs - t.secs;
-	if (difference == 0)
+	if (secs - t.secs == 0)
 	{
 		return 0;
 	}
-	else if (difference > 0)
-	{
-		return -1;
-	}
-	else
+	else if (secs - t.secs < 0)
 	{
 		return 1;
 	}
-}
+	else
+	{
+		return -1; 
+	}
+} //ends Compare Time
+
+/* To String */
+string Time::toString() const
+{
+	tm* gmtm = gmtime(&secs);
+	char* dateTime = asctime(gmtm);
+	return dateTime;
+} //ends To String
