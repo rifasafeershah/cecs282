588000196e0838809b8e9de6259188257f68c4ed
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-13 22:25:42 -0800

    Update student.cpp

--- assignment2/student.cpp
+++ assignment2/student.cpp
@@ -8,21 +8,22 @@ using namespace std;
 /* Default Constructor */
 Student:: Student()
 {
-	level = "Recruit";
-	id = "000000000";
-	name = "Rifa Fifa";
-	status = "money";
-	date_of_action = 0;
-}
+	name = ""; //name of the student
+	id = ""; //student id number
+	level = ""; //level of the student - FRESHMAN, SOPHOMORE, JUNIOR, SENIOR
+	status = ""; //status of the student - ENROLLED, DROPPED, ADDED
+	date_of_action = 0; //date of action
+} //ends Default Constructor
 
 /* Overload Constructor */
 Student :: Student(string id, string name, string level, Time date_of_action)
 {
-	this->id = id;
 	this->name = name;
+	this->id = id;
 	this->level = level;
+	//this->status = status;
 	this->date_of_action = date_of_action;
-}
+} //ends Overload Constructor
 
 
 /* Copy Constructor */
@@ -32,7 +33,7 @@ Student::Student(const Student& s)
 	(*this).id = s.id;
 	(*this).level = s.level;
 	(*this).date_of_action = s.date_of_action;
-}
+} //ends Copy Constructor
 
 /* Assignment Operator */
 Student& Student :: operator=(const Student& s)
@@ -42,42 +43,44 @@ Student& Student :: operator=(const Student& s)
 	(*this).level = s.level;
 	(*this).date_of_action = s.date_of_action;
 	return (*this);
-}
-
-/* Get Level */
-string Student::getLevel() const {
-	return level;
-}
-
-/* Get ID */
-string Student::getId() const {
-	return id;
-}
+} //ends Assignment Operator
 
 /* Get Name */
 string Student::getName() const {
-	return name;
-}
+	return name; //returns name of the student
+} //Ends Get Name
+
+/* Get ID */
+string Student::getId() const {
+	return id; //returns id of the student
+} //ends Get ID
+
+/* Get Level */
+string Student::getLevel() const {
+	return level; //returns level of the student
+} //ends Get Level
 
 /* Get Date of Action */
 string Student::getDateOfAction() const {
 	return date_of_action.toString();
-}
+} //ends Get Date of Action
+
+/* Set Time of Action */
+void Student::setTimeOfAction(Time act) {
+	date_of_action = act;
+} //ends Set Time of Action
 
 /* Get Time of Action */
-Time Student::getTimeOfAction() const {//return date_of_action in seconds
-	return date_of_action;
-}
+Time Student::getTimeOfAction() const {
+	return date_of_action; //returns date of action
+} //ends Get Time of Action
 
 /* Get Status */
 string Student::getStatus() const {
-	return status;
-}
+	return status; //returns status of the student
+} //ends Get Status
 
+/* Set Status*/
 void Student::setStatus(string stat) {
-	status = stat;
-}
-
-void Student::setTimeOfAction(Time act) {
-	date_of_action = act;
-}
+	status = stat; //sets status of the student
+} //ends Set Status
