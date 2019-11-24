a23de4a48883593b1cfbd1b91a58b8874c73af7a
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-24 13:37:14 -0800

    Update Box.cpp

--- class notes/Class Box/Box with array/Box.cpp
+++ class notes/Class Box/Box with array/Box.cpp
@@ -1,1 +1,31 @@
+#include "Box.h"
+#include <iostream>
+using namespace std;
+Box :: Box() //default constructor
+{
+  cout << "Default constructor" << endl;
+  i = 1;
+}
+Box :: Box(int x) //overloaded constructor
+{
+  cout << "Overloaded constructor" << endl;
+  i = x;
+}
 
+Box :: Box(const Box& a)
+{
+  cout << "Copy constructor" << endl;
+  i = a.i;
+}
+int Box :: getSide() const
+{
+  return i;
+}
+void Box :: setSide(int x)
+{
+  i = x;
+}
+int Box :: area() const
+{
+  return 6 * i * i;
+}
