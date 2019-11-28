145d05bc0a75aeaa01f7e9a6ec3a0ed09a41fce8
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-27 17:30:40 -0800

    Update DoublyLinkedList.h

--- assignment3/DoublyLinkedList.h
+++ assignment3/DoublyLinkedList.h
@@ -4,4 +4,17 @@ class DoublyLinkedList
     protected:
         Node<T>* head; //pointer to the first node of a doubly linked list
         Node<T>* tail; //pointer to the last node of a doubly linked list
-        int size;
+        int size; //size of a doubly linked list
+    public:
+        Doubly LinkedList();
+        void addFront(T d); //add a node at the beginning of a doubly linked list
+        void popFront(); //remove a node at the beginning of a doubly linked list
+        void addBack(T d); //add a node at the end of a doubly linked list
+        void popBack(); //remove a node at the end of a doubly linked list
+        void addNode(T d, Node<T>* iter); //add a node in general
+        void deleteNode(T d, Node<T>* iter); //delete a node in general
+        Node<T>* findNode(T d); //return a pointer to a node in a doubly linked list and return NULL otherwise
+        int getSize() const; //return size of a doubly linked list
+        bool isEmpty() const; //return true if empty and false otherwise
+        void displayList() const; //display a doubly linked list
+};
