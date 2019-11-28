574d61d35d29b4d1d6d5f62876e36c90c188f9c7
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-27 17:23:16 -0800

    Update Complex.h

--- assignment3/Complex.h
+++ assignment3/Complex.h
@@ -3,4 +3,19 @@ class Complex
     public:
         Complex();
         Complex(int r, int i, int d);
-        int re;
+        int re; //real part of a complex number
+        int im; //imaginary part of a complex number
+        int dem; //denominator part of a complex number
+        string toString() const; //Format [re + imi]/dem
+};
+
+//Overloading operator for easy arithmetic
+Complex operator+(Complex a, Complex b);
+Complex operator-(Complex a, Complex b);
+Complex operator*(Complex a, Complex b);
+Complex operator/(Complex a, Complex b);
+ostream& operator<< (ostream& stream, Complex a);
+
+//Find gcd and lcm to reduce fraction and add fraction
+int gcd(int a, int b);
+int lcm(int a, int b);
