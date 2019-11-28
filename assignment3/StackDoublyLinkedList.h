11c50a3b0324041accb86b93075bc0fe8410b135
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-27 17:35:09 -0800

    Update StackDoublyLinkedList.h

--- assignment3/StackDoublyLinkedList.h
+++ assignment3/StackDoublyLinkedList.h
@@ -1,1 +1,8 @@
-
+template <class T>
+class StackDoublyLinkedList : public DoublyLinkedList<T>
+{
+  public:
+    StackDoublyLinkedList();
+    void displayStack() const; //Display a stack
+    T getTop() const; //return element at the top of a stack
+};
