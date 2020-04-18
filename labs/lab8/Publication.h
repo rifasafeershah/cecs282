3bbfe846d28a5a89d25e2b6e4c9574d2d7f90bc7
Rifa Safeer Shah <rifashah28@gmail.com>
2020-04-18 12:59:24 -0700

    Update Publication.h

--- labs/lab8/Publication.h
+++ labs/lab8/Publication.h
@@ -1,1 +1,26 @@
+//Rifa Safeer Shah
+//CECS 282 LAB 8
+//Publication.h
 
+#ifndef PUBLICATION_PUBLICATION_H
+#define PUBLICATION_PUBLICATION_H
+
+#include <iostream>
+
+using namespace std;
+
+class Publication {
+	/* Create public constructors */
+	public:
+		//Declare two functions getData and putData
+		void getData();
+		void putData();
+	
+	/* Create protected constructors */
+	protected:
+		//Declare title and price
+		string title; //declare title as string type
+		float price; //declare price as float type
+};
+
+#endif //PUBLICATION_PUBLICATION_H
