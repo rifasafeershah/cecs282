b5f1b2cae87b118dbf6f8cd287ccd7fc8f620a5c
Rifa Safeer Shah <rifashah28@gmail.com>
2020-04-19 16:00:54 -0700

    Update circleType.h

--- labs/lab7/circleType.h
+++ labs/lab7/circleType.h
@@ -1,1 +1,26 @@
+//Rifa Safeer Shah
+//CECS 282 LAB 7 PROBLEM 1
+//circleType.h
 
+//Class circleType
+
+#ifndef H_CircleType
+#define H_CircleType
+
+#include "cecs282lab7pointType.h"
+
+class circleType: public pointType
+{
+	public:
+		void print() const;
+		void setRadius(double r);
+		double getRadius() const;
+		double getCircumference() const;
+		double getArea() const;
+		circleType(double x = 0.0, double y = 0.0, double r = 0.0);
+		
+	protected:
+		double radius;
+};
+
+#endif
