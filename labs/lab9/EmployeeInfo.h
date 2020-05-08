047d31d849ef4d8e4bfd136b9281e57e7ea129b9
Rifa Safeer Shah <rifashah28@gmail.com>
2020-05-08 14:35:32 -0700

    Create EmployeeInfo.h

---
+++ labs/lab9/EmployeeInfo.h
@@ -0,0 +1,15 @@
+//Rifa Safeer Shah
+//CECS 282 LAB 9
+//EmployeeInfo.h
+
+#ifndef EmployeeInfo_hpp
+#define EmployeeInfo_hpp
+
+#include <iostream>
+
+using namespace std;
+
+const double FACULTY_MONTHLY_SALARY = 5000.00;
+const int STAFF_MONTHLY_HOURS_WORKED = 160;
+
+#endif /* Header_h */
